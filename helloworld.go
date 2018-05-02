bb8e4de20df7187496dd667a59a923867546d3d6
sachinkarale <sachin.karale@reactiveworks.in>
2018-05-02 17:58:53 +0530

    test

--- helloworld.go
+++ helloworld.go
@@ -21,10 +21,6 @@ func main() {
 
 func hello(w http.ResponseWriter, req *http.Request) {
 	log.Printf("%s %s\n", req.Proto, req.URL)
-<<<<<<< HEAD
-	fmt.Fprintln(w, "HELLO ! It's not working")
-=======
-	fmt.Fprintln(w, "HELLO ! It's jenkins autobuild testing")
->>>>>>> 5777ed777fc75905b41ae4a2f02814f1cdeeddb9
+	fmt.Fprintln(w, "HELLO JUBER!")
 	fmt.Fprintln(w, "See the documentation at http://docs.deis.io/ for more information.")
 }
