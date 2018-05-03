c3c292845ddabe0e27dd03a8079eee69f1fbefae
sachinkarale <sachin.karale@reactiveworks.in>
2018-05-03 14:52:11 +0530

    test

--- helloworld.go
+++ helloworld.go
@@ -21,6 +21,6 @@ func main() {
 
 func hello(w http.ResponseWriter, req *http.Request) {
         log.Printf("%s %s\n", req.Proto, req.URL)
-        fmt.Fprintln(w, "HELLO Mozilla , Good Afternoon!")
+        fmt.Fprintln(w, "HELLO ,This is Mozilla , Good Afternoon!")
         fmt.Fprintln(w, "See the documentation at http://docs.deis.io/ for more information.")
 }
